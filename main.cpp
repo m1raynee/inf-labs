09d1e7a0e5ab6a2b2ce81c832f0ab9f2de20ce8a
miray <71838742+m1raynee@users.noreply.github.com>
2024-12-19 23:18:25 +0300

    add factorial of 5

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world! Did you know that 5! is "
+    << std::tgamma(5-1) << std::endl;
     return 0;
 }
